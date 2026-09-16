ef254553db8799bfa5c5b1d5bfa528e2971c9df7
Lorenzo Baroni <lorenzob18@hotmail.com>
2026-09-16 00:27:22 -0300

    cleanup: remove unnecessary world bootstrap hooks

--- src/SharedDiscovery/Patches/NetworkLifecyclePatch.cs
+++ src/SharedDiscovery/Patches/NetworkLifecyclePatch.cs
@@ -1,5 +1,4 @@
 using HarmonyLib;
-using SharedDiscovery.Discovery;
 
 namespace SharedDiscovery.Patches
 {
@@ -9,50 +8,6 @@ namespace SharedDiscovery.Patches
         private static void Postfix()
         {
             Plugin.Network.TryRegister();
-        }
-    }
-
-    [HarmonyPatch(typeof(ZNet), "LoadWorld")]
-    internal static class ZNetLoadWorldPatch
-    {
-        private static void Postfix()
-        {
-            if (ZNet.instance == null || !ZNet.instance.IsServer())
-            {
-                return;
-            }
-
-            Plugin.DebugLog("SharedDiscovery host world bootstrap after ZNet.LoadWorld.");
-            if (WorldContext.TryGetCurrentWorldId(out string worldId))
-            {
-                Plugin.DebugLog($"World UID: {worldId}.");
-            }
-
-            Plugin.Discovery.EnsureWorldLoaded();
-        }
-    }
-
-    [HarmonyPatch(typeof(ZNet), "RPC_PeerInfo")]
-    internal static class ZNetPeerInfoPatch
-    {
-        private static void Postfix()
-        {
-            if (ZNet.instance == null || ZNet.instance.IsServer())
-            {
-                return;
-            }
-
-            bool playerReady = Plugin.Session.IsLocalPlayerReady;
-            bool worldContextValid = WorldContext.TryGetCurrentWorldId(out _);
-            Plugin.DebugLog($"Client world info received through RPC_PeerInfo. Player ready: {playerReady}. World context valid: {worldContextValid}.");
-
-            if (!playerReady || !worldContextValid)
-            {
-                return;
-            }
-
-            Plugin.DebugLog("Snapshot retry triggered after RPC_PeerInfo.");
-            Plugin.Network.RequestSnapshot();
         }
     }
 
